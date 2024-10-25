542da6705293301f4d9389802a9a8a55bc019e89
Luis <lfernandotc@hotmail.com>
2024-10-25 09:42:11 -0300

    Configuração setup

---
+++ src/main/java/com/cliente/ws/rasmooplus/WsRasmoPlusApplication.java
@@ -0,0 +1,13 @@
+package com.cliente.ws.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRasmoPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsRasmoPlusApplication.class, args);
+	}
+
+}
